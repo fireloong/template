2b92ba7996c872c895e58dd83173047bf29a9ee9
火龙 <fireloong@foxmail.com>
2022-04-13 20:26:59 +0800

    feat(router): 增加路由组件及演示

---
+++ src/pages/About.vue
@@ -0,0 +1,8 @@
+<template>
+  <h2>About</h2>
+  <p>
+    <router-link to="/">Home</router-link> | <router-link to="/about">About</router-link>
+  </p>
+</template>
+
+<script setup lang="ts"></script>
---
+++ src/pages/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <h2>Home</h2>
+  <p>
+    <router-link to="/">Home</router-link> | <router-link to="/about">About</router-link>
+  </p>
+  <img alt="Vue logo" src="@/assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+</template>
+
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from '@/components/HelloWorld.vue'
+</script>
